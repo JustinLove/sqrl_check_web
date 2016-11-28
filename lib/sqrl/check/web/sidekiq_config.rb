3eb8f4d5738cfb01efc50058f7dfc397fefa1159
Justin Love <git@JustinLove.name>
2016-11-28 11:44:17 -0600

    limit redis connections

--- lib/sqrl/check/web/sidekiq_config.rb
+++ lib/sqrl/check/web/sidekiq_config.rb
@@ -4,12 +4,14 @@ require 'autoscaler/heroku_platform_scaler'
 require 'sidekiq'
 
 Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
   config.client_middleware do |chain|
     chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuPlatformScaler.new
   end
 end
 
 Sidekiq.configure_server do |config|
+  config.redis = { :size => 8 }
   config.server_middleware do |chain|
     chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuPlatformScaler.new, 60) # 60 second timeout
   end
